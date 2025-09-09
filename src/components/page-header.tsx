c76306c27829ed9d14af904977fd486a88d924e3
HamzaElshammaa <mozamiza@gmail.com>
2025-09-09 11:06:50 +0300

    feat: update breadcrumb component styles for improved visibility and consistency

--- src/components/page-header.tsx
+++ src/components/page-header.tsx
@@ -1,29 +1,18 @@
-import { ArrowLeft } from 'lucide-react';
+
 import { PropsWithChildren, ReactNode } from 'react';
-import { Button } from './ui/button';
 
-interface IPageHeaderProps extends PropsWithChildren {
-  back(): void;
-  title: ReactNode;
-}
 
-export function PageHeader({ back, title, children }: IPageHeaderProps) {
+
+export function PageHeader({ children }: PropsWithChildren) {
   return (
     <header
-      className="flex justify-between items-center border-b pr-9 text-white"
+      className="flex justify-between items-center bg-text-title-invert p-5"
       style={{ backgroundColor: '#171c42' }}
     >
-      <div className="flex items-center ">
-        <div className="flex items-center border-r p-1.5">
-          <Button variant="ghost" size="icon" onClick={back}>
-            <ArrowLeft className="w-5 h-5" />
-          </Button>
-        </div>
-        <div className="p-4">
-          <h1 className="text-2xl font-semibold tracking-tight">{title}</h1>
-        </div>
-      </div>
+
+     
       {children}
+
     </header>
   );
 }
